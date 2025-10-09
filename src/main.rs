d27978bfc3456ba8209d789cdbadf9e6bd4d32a5
simbullar <simbullar@gmail.com>
2025-10-09 09:47:35 +0200

    started on the game

--- src/main.rs
+++ src/main.rs
@@ -16,25 +16,65 @@ fn main() {
         (WIDTH, HEIGHT),
         "Space Invaders+!!",
         Style::CLOSE,
-        &ContextSettings::default(),
-    );
+        &Default::default(),
+    )
+    .unwrap();
     window.set_vertical_sync_enabled(true);
 
-    // all of those are placeholders, in the future i am going to replace this with good images
+    // ------------------------------------ MAIN MENU DEFINITIONS ------------------------------------
 
-    // buttonPlay definitions
+    // some of these are placeholders, in the future i am going to replace this with good images
+    // background definitions
+    let background_texture =
+        Texture::from_file("assets/background.png").expect("Failed to load background texture");
+    let mut background = Sprite::new();
+    background.set_texture(&background_texture, false);
+
+    // game_title definitions
+    let game_title_texture =
+        Texture::from_file("assets/gameTitle.png").expect("Failed to load background texture");
+    let mut game_title = Sprite::new();
+    game_title.set_texture(&game_title_texture, false);
+    game_title.set_position(Vector2f::new(130.0, 220.0));
+
+    // button_play definitions
     let mut button_play = RectangleShape::new();
     button_play.set_size(Vector2f::new(200.0, 50.0));
     button_play.set_fill_color(Color::rgb(255, 255, 255));
-    button_play.set_position(Vector2f::new(300.0, 350.0));
-    let button_play_bounds = button_play.global_bounds();
+    button_play.set_position(Vector2f::new(290.0, 350.0));
 
     //buttonSettings definitions
     let mut button_settings = RectangleShape::new();
     button_settings.set_size(Vector2f::new(40.0, 40.0));
     button_settings.set_fill_color(Color::rgb(255, 255, 255));
-    button_settings.set_position(Vector2f::new(WIDTH as f32 - 40.0, 0.0));
-    let button_settings_bounds = button_settings.global_bounds();
+    button_settings.set_position(Vector2f::new(WIDTH as f32 - 40.0, 1.0));
+
+    // ------------------------------------ GAME DEFINITIONS ---------------------------------------
+    // player definitions
+    let mut player = RectangleShape::new();
+    player.set_size(Vector2f::new(40.0, 40.0));
+
+    //ball definitions
+    let ball_texture =
+        Texture::from_file("assets/ball.png").expect("Failed to load background texture");
+    let mut ball = Sprite::new();
+    ball.set_texture(&ball_texture, false);
+    ball.set_scale(0.3);
+    ball.set_position(Vector2f::new(WIDTH as f32 - 170.0, HEIGHT as f32 - 112.0));
+
+    // stand definitions
+    let stand_texture =
+        Texture::from_file("assets/stand.png").expect("Failed to load background texture");
+    let mut stand = Sprite::new();
+    stand.set_texture(&stand_texture, false);
+    stand.set_scale(0.7);
+    stand.set_position(Vector2f::new(WIDTH as f32 - 200.0, HEIGHT as f32 - 115.0));
+
+    // button definitions
+    let button_off_texture =
+        Texture::from_file("assets/buttonOff.png").expect("Failed to load background texture");
+    let mut button = Sprite::new();
+    button.set_texture(&button_off_texture, false);
 
     loop {
         // events
@@ -45,14 +85,14 @@ fn main() {
                     return;
                 }
                 Event::MouseButtonReleased { button, x, y } => {
-                    if button == mouse::Button::Left {
-                        if button_play_bounds.contains(Vector2f::new(x as f32, y as f32)) {
+                    if button == mouse::Button::Left && !has_game_started && !settings_opened {
+                        let mouse_pos =
+                            window.map_pixel_to_coords(Vector2i::new(x, y), &window.view());
+
+                        if button_play.global_bounds().contains(mouse_pos) {
                             has_game_started = true;
-                        } else if button_settings_bounds.contains(Vector2f::new(x as f32, y as f32))
-                        {
+                        } else if button_settings.global_bounds().contains(mouse_pos) {
                             settings_opened = true;
-                        } else {
-                            print!("hmm");
                         }
                     }
                 }
@@ -62,17 +102,37 @@ fn main() {
 
         // drawing
         window.clear(Color::BLACK);
-        if !has_game_started {
-            // menu screen
+        if settings_opened {
+            // settings screen
+            window.draw(&background);
+        } else if !has_game_started {
+            // menu
+            window.draw(&background);
+            window.draw(&game_title);
             window.draw(&button_play);
             window.draw(&button_settings);
-        } else if settings_opened {
-            // settings screen
-        } else {
-            // play again screen
-            // 
-        }
+        } else if !is_game_over {
+            // game
 
+            // ----------- GAME LOGIC -----------
+            if mouse::Button::Left.is_pressed() {
+                // do
+            } else if mouse::Button::Right.is_pressed() {
+                // change to variant
+            } else {
+                // change everything back
+                button.set_texture(&button_off_texture, false);
+            }
+
+            // Draws for the game screen
+            window.draw(&background);
+
+            window.draw(&stand);
+            window.draw(&ball)
+        } else {
+            // game over
+            window.draw(&background);
+        }
         window.display();
     }
 }
